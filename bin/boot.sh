92f27c168caf0c142c672173638d2c43fa2c92c6
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2014-12-05 14:51:30 +0000

    Generate mt-config.cgi just before starting app.

---
+++ bin/boot.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [[ "$DATABASE_URL" =~ ^postgres://([^:@/]+):([^:@/]+)@([^:@/]+):([^:@/]+)/([^:@/]+)$ ]]; then
+  dbuser=${BASH_REMATCH[1]}
+  dbpass=${BASH_REMATCH[2]}
+  dbhost=${BASH_REMATCH[3]}
+  dbport=${BASH_REMATCH[4]}
+  db=${BASH_REMATCH[5]}
+fi
+
+cat << _CONFIG_ > /app/mt-config.cgi
+CGIPath /
+StaticWebPath /mt-static
+StaticFilePath /app/mt-static
+
+ObjectDriver DBI::postgres
+DBHost $dbhost
+DBPort $dbport
+DBUser $dbuser
+DBPassword $dbpass
+Database $db
+
+DefaultLanguage ja
+
+ImageDriver Imager
+_CONFIG_
