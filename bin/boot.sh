d15d9feee27a1f5509c529c1b1891f26834a1740
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2014-12-08 12:33:52 +0900

    Ready for use MT in zip.

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,28 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ "$DATABASE_URL" =~ ^postgres://([^:@/]+):([^:@/]+)@([^:@/]+):([^:@/]+)/([^:@/]+)$ ]]; then
-  dbuser=${BASH_REMATCH[1]}
-  dbpass=${BASH_REMATCH[2]}
-  dbhost=${BASH_REMATCH[3]}
-  dbport=${BASH_REMATCH[4]}
-  db=${BASH_REMATCH[5]}
+if [ $MT_ZIP ]; then
+  curl $MT_ZIP
+else
+  mv ./mt/* ./
 fi
 
-cat << _CONFIG_ > /app/mt-config.cgi
-CGIPath /mt/
-StaticWebPath /mt-static
-StaticFilePath /app/mt-static
+bash generate-mt-config.sh
 
-ObjectDriver DBI::postgres
-DBHost $dbhost
-DBPort $dbport
-DBUser $dbuser
-DBPassword $dbpass
-Database $db
+perl -Mlib=./local/lib/perl5 ./local/bin/starman --pid ./mt.pid ./mt.psgi &
 
-ImageDriver Imager
-
-BaseSitePath /app/html
-
-PIDFilePath /app/mt.pid
-_CONFIG_
+vendor/bin/heroku-php-apache2 -c ./httpd.conf
---
+++ bin/generate-mt-config.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if [[ "$DATABASE_URL" =~ ^postgres://([^:@/]+):([^:@/]+)@([^:@/]+):([^:@/]+)/([^:@/]+)$ ]]; then
+  dbuser=${BASH_REMATCH[1]}
+  dbpass=${BASH_REMATCH[2]}
+  dbhost=${BASH_REMATCH[3]}
+  dbport=${BASH_REMATCH[4]}
+  db=${BASH_REMATCH[5]}
+fi
+
+cat << _CONFIG_ > /app/mt-config.cgi
+CGIPath /mt/
+StaticWebPath /mt-static
+StaticFilePath /app/mt-static
+
+ObjectDriver DBI::postgres
+DBHost $dbhost
+DBPort $dbport
+DBUser $dbuser
+DBPassword $dbpass
+Database $db
+
+ImageDriver Imager
+
+BaseSitePath /app/html
+
+PIDFilePath /app/mt.pid
+_CONFIG_
