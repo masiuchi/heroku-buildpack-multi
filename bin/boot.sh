5f736dce2dfdfb09b34cbf5f3d70603c11fd1f08
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2014-12-08 18:08:48 +0900

    Moved process of copying index.html.

--- bin/boot.sh
+++ bin/boot.sh
@@ -16,6 +16,11 @@ if [ ! -f mt-config.cgi ]; then
   ./generate-mt-config.sh
 fi
 
+mkdir html
+cp index.html html/
+sed -i -e "s/mt\.cgi/\/mt\/mt\.cgi/g" html/index.html
+sed -i -e "s/mt-check\.cgi/\/mt\/mt-check\.cgi/g" html/index.html
+
 perl -Mlib=./local/lib/perl5 ./local/bin/starman --pid ./mt.pid ./mt.psgi &
 
 vendor/bin/heroku-php-apache2 -c ./httpd.conf
