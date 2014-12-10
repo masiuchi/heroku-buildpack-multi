8cba5b10cd7b8bec9dbe8669ed9d2eb66234d386
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2014-12-10 16:57:05 +0900

    Do not use URI::Split. #6

--- mt/addons/Heroku.pack/lib/Heroku/KeepUpHeroku.pm
+++ mt/addons/Heroku.pack/lib/Heroku/KeepUpHeroku.pm
@@ -2,24 +2,18 @@ package Heroku::KeepUpHeroku;
 use strict;
 use warnings;
 
-use URI::Split qw/ uri_split uri_join /;
-
 sub keep_up {
     my $site
         = MT->model('blog')
         ->load( { class => '*' }, { sort => 'created_on', limit => 1 } )
         or return;
 
-    my ( $schema, $auth ) = uri_split( $site->site_url );
-    my $url = uri_join(
-        $schema, $auth,
-        MT->config->AdminCGIPath,
-        MT->config->AdminScript,
-    );
+    my ($url) = $site->site_url =~ m{^(https?://(?:[^/]+))};
+    my $path = MT->config->AdminCGIPath;
+    $path =~ s/^\/|\/$//g;
+    $url = join '/', ( $url, $path, MT->config->AdminScript );
 
-MT->log( 'AdminCGIPath: '. MT->config->AdminCGIPath );
-MT->log( 'AdminScript: ' . MT->config->AdminScript );
-MT->log( 'url: ' . $url );
+MT->log( $url );
 
     `curl $url`;
 }
