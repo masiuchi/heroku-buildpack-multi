29e4bc8dbbd3f084a428c0925eb8c4d804ff6252
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2014-12-10 16:25:55 +0900

    Use site url. #6

---
+++ mt/addons/Heroku.pack/lib/Heroku/KeepUpHeroku.pm
@@ -0,0 +1,25 @@
+package Heroku::KeepUpHeroku;
+use strict;
+use warnings;
+
+use URI::Split qw/ uri_split uri_join /;
+
+sub keep_up {
+    my $site
+        = MT->model('blog')
+        ->load( { class => '*' }, { sort => 'created_on', limit => 1 } )
+        or return;
+
+    my ( $schema, $auth ) = uri_split( $site->site_url );
+    my $url = uri_join(
+        $schema, $auth,
+        MT->config->AdminCGIPath,
+        MT->config->AdminScript,
+    );
+
+MT->log( $url );
+
+    `curl $url`;
+}
+
+1;
